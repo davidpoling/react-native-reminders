e302793b611ef1aad79ff34f8f58747115a0eabe
davidpoling <soraspar78@gmail.com>
2020-02-15 12:06:58 -0500

    moved reminders app up a level since the full path was unnecessary

---
+++ Reminders-App/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View, StyleSheet} from 'react-native';
+import Header from './components/Header';
+import RemindersScreen from './screens/RemindersScreen';
+import {RemindersProvider} from './context/RemindersContext';
+import CalendarScreen from './screens/CalendarScreen';
+
+export default App = () => {
+  const Stack = createStackNavigator();
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Header title="Reminders" />
+      <RemindersProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{headerShown: false}}
+            initialRouteName="Reminders Screen">
+            <Stack.Screen name="Reminders Screen" component={RemindersScreen} />
+            <Stack.Screen name="Calendar Screen" component={CalendarScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </RemindersProvider>
+    </View>
+  );
+};
---
+++ Reminders-App/screens/CalendarScreen.js
@@ -0,0 +1,119 @@
+import React, {useState, useContext, useEffect} from 'react';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {Agenda} from 'react-native-calendars';
+import RemindersContext from '../context/RemindersContext';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+export default CalendarScreen = ({navigation}) => {
+  const [calendarItems, setCalendarItems] = useState({});
+  const remindersContext = useContext(RemindersContext);
+
+  useEffect(() => {
+    const reminders = remindersContext.reminders;
+    if (reminders && reminders.length > 0) {
+      setCalendarItems(generateCalendarItems(reminders));
+    }
+  }, [remindersContext]);
+
+  function generateCalendarItems(reminders) {
+    let newCalendarItems = {};
+
+    reminders.forEach(reminder => {
+      const key = generateKeyFromReminder(reminder);
+      if (newCalendarItems[key]) {
+        newCalendarItems[key].push({name: getFormattedReminderText(reminder)});
+      } else {
+        newCalendarItems[key] = [{name: getFormattedReminderText(reminder)}];
+      }
+    });
+
+    return newCalendarItems;
+  }
+
+  function generateKeyFromReminder(reminder) {
+    const year = reminder.dateTime.getFullYear();
+    const day = reminder.dateTime.getDate();
+    let month = reminder.dateTime.getMonth() + 1;
+
+    // If the month is < 10, add a prefixed zero.
+    // The <Agenda /> component cannot read months without a prefixed zero.
+    if (month < 10) {
+      month = '0' + month;
+    }
+
+    return year + '-' + month + '-' + day;
+  }
+
+  function getFormattedReminderText(reminder) {
+    return reminder.text + ' - ' + reminder.dateTimeString;
+  }
+
+  function renderItem(item) {
+    return (
+      <TouchableOpacity style={styles.item}>
+        <Text>{item.name}</Text>
+      </TouchableOpacity>
+    );
+  }
+
+  function renderEmptyDate() {
+    return (
+      <View style={styles.emptyDate}>
+        <Text>This is an empty date!</Text>
+      </View>
+    );
+  }
+
+  function renderEmptyData() {
+    return <View />;
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    screenButton: {
+      backgroundColor: '#3399ff',
+      padding: 5,
+      margin: 5,
+    },
+    screenButtonText: {
+      color: 'white',
+      fontSize: 20,
+      textAlign: 'center',
+    },
+    item: {
+      backgroundColor: 'white',
+      flex: 1,
+      borderRadius: 5,
+      padding: 10,
+      marginRight: 10,
+      marginTop: 17,
+    },
+    emptyDate: {
+      height: 15,
+      flex: 1,
+      paddingTop: 30,
+    },
+  });
+
+  return (
+    <View style={styles.container}>
+      <Agenda
+        items={calendarItems}
+        renderItem={renderItem}
+        renderEmptyDate={renderEmptyDate}
+        renderEmptyData={renderEmptyData}
+      />
+      <TouchableOpacity
+        style={styles.screenButton}
+        onPress={() => {
+          navigation.navigate('Reminders Screen');
+        }}>
+        <Text style={styles.screenButtonText}>
+          <Icon name="list" size={20} />
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
