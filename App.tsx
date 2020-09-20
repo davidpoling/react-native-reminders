7b41a07339a27d164f9a8246ebef88e9e2dcc0df
David Poling <soraspar78@gmail.com>
2020-09-20 15:21:20 -0400

    Removed stack nav stuff

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {View, StyleSheet} from 'react-native';
 import RemindersScreen from './screens/RemindersScreen';
 import {RemindersProvider} from './context/RemindersContext';
 
 export default function App() {
-  const Stack = createStackNavigator();
   const Tab = createBottomTabNavigator();
 
   const styles = StyleSheet.create({
@@ -22,7 +20,7 @@ export default function App() {
       <RemindersProvider>
         <NavigationContainer>
           <Tab.Navigator>
-            <Stack.Screen name="Reminders Screen" component={RemindersScreen} />
+            <Tab.Screen name="Reminders Screen" component={RemindersScreen} />
           </Tab.Navigator>
         </NavigationContainer>
       </RemindersProvider>
