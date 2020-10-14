a304a70ea6b49a3a57fd98f659b6a097a5ff444c
Dave <soraspar78@gmail.com>
2020-10-13 20:58:56 -0400

    Added prod files and updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,28 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import {Platform} from 'react-native'
+import {Platform} from 'react-native';
 
 export default function Header({title}: any) {
   const styles = StyleSheet.create({
     header: {
       height: 60,
       padding: 15,
-      backgroundColor: '#314b7f',
     },
     iosheader: {
       height: 90,
       padding: 15,
       paddingTop: 45,
-      backgroundColor: '#314b7f',
     },
     text: {
-      color: '#fff',
+      color: '#314b7f',
       fontSize: 23,
+      fontWeight: 'bold',
     },
   });
 
   return (
     // Because iPhones have that stupid notch and the header starts at the VERY top of the screen...
-    <View style={Platform.OS === "ios" ? styles.iosheader : styles.header}>
+    <View style={Platform.OS === 'ios' ? styles.iosheader : styles.header}>
       <Text style={styles.text}>{title}</Text>
     </View>
   );
